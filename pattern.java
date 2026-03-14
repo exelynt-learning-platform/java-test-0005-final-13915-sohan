a25a803f1bc6d84b9fbfd4aa8eea7cb10f70485a
SohanWebtrix <Sohan.Nikam@mkvisolutions.com>
2026-03-14 14:56:38 +0530

    pattern printing assignment

---
+++ pattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class pattern {
+    public static void main(String args[]) {
+        int n = 5;
+        int start = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(start + " ");
+                start++;
+
+            }
+            System.out.println();
+        }
+    }
+}
